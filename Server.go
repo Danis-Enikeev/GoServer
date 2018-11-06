1c7634dcfa1226872f742923641c172068d750d3
Danis <danisen98@gmail.com>
2018-11-06 23:59:49 +0300

    added the standart query handler

--- Server.go
+++ Server.go
@@ -12,19 +12,26 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const MIN = 1
-const MAX = 100
-
 type TCPMessage struct {
 	action   int    `json:"action"`
 	login    string `json:"login"`
-	password string `json:"password"`
 	roomcode string `json:"roomcode"`
 	data     string `json:"data"`
 }
+type UserRoom struct {
+	Room     string
+	RoomName string
+	Role     int
+	Image1   string
+	Image2   string
+	Image3   string
+	Events   []Event
+}
 
-func random() int {
-	return rand.Intn(MAX-MIN) + MIN
+type Event struct {
+	Event  string
+	Author string
+	Flag   bool
 }
 
 /*func handleConnection(c net.Conn) {
@@ -53,8 +60,70 @@ func handleServerConnection(c net.Conn, db *sql.DB) {
 	var msg TCPMessage
 	err := d.Decode(&msg)
 	fmt.Println(msg, err)
-	sqlStatement := "INSERT INTO logs (roomcode, login, password) VALUES ($1, $2, $3)"
-	_, err = db.Exec(sqlStatement, msg.roomcode, msg.login, msg.data)
+	switch msg.action {
+	case 0:
+		sqlStatement := "SELECT room, status FROM userRoom WHERE user = $1"
+
+		rows, err := db.Query(sqlStatement, msg.login)
+		if err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+		userRooms := []UserRoom{}
+
+		var room string
+		var role int
+		for rows.Next() {
+			userRoom := UserRoom{}
+			err = rows.Scan(&room, &role)
+			if err != nil {
+				panic(err)
+			}
+			userRoom.Room = room
+			userRoom.Role = role
+
+			sqlStatement = "SELECT roomName, image1, image2, image3 FROM roomData WHERE room = $1"
+			row := db.QueryRow(sqlStatement, room)
+			err := row.Scan(&userRoom.RoomName, &userRoom.Image1, &userRoom.Image2, &userRoom.Image3)
+			if err != nil {
+				if err == sql.ErrNoRows {
+					fmt.Println("No rows found")
+				} else {
+					panic(err)
+				}
+			}
+			sqlStatement = `SELECT event, creator, status FROM roomData WHERE room = $1`
+			rows, err := db.Query(sqlStatement, room)
+			if err != nil {
+				panic(err)
+			}
+			defer rows.Close()
+
+			for rows.Next() {
+				event := Event{}
+				err = rows.Scan(&event.Event, &event.Author, &event.Flag)
+				if err != nil {
+					panic(err)
+				}
+				userRoom.Events = append(userRoom.Events, event)
+
+			}
+			err = rows.Err()
+			if err != nil {
+				panic(err)
+			}
+			userRooms = append(userRooms, userRoom)
+		}
+
+		err = rows.Err()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(userRooms)
+		answer, _ := json.Marshal(userRooms)
+		c.Write(answer)
+	}
+
 	c.Close()
 
 }
